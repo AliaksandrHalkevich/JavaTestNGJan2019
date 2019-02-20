fa9617d57844b58cf48ee1bec9477ced6351a808
Sasha <qagalkevich@gmail.com>
2019-02-20 14:44:05 +0300

    day 2 tests added

--- src/test/java/day1/Day1CodingExercises.java
+++ src/test/java/day1/Day1CodingExercises.java
@@ -117,54 +117,6 @@ public class Day1CodingExercises {
             System.out.println("not equal to 3");
         }
     }
-
-
-    @Test
-    public void test012() {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
-
-        int i;
-        for (i = 0; i < 4; i++) {
-            System.out.println(arr[i]);
-        }
-    }
 }
 
-    class main {
-        public static void main(String[] args) {
-            byte z = 0;
-            short a = 3;
-            int b = 11;
-            char d = ' ';
-            float x = 2.0f;
-            boolean f = true;
-            String output = "H" + a + b + z + d + "w" + z + "r" + b + "d" + d + x + d + f;
-            System.out.println(output);
-        }
-    }
 
-    class Main {
-        public static void main(String[] args) {
-                int[] numbers = {
-                951, 402, 984, 651, 360, 69, 408, 319, 601, 485, 980, 507, 725, 547, 544,
-                615, 83, 165, 141, 501, 263, 617, 865, 575, 219, 390, 984, 592, 236, 105, 942, 941,
-                386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345,
-                399, 162, 758, 219, 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217,
-                815, 67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379, 843, 831, 445, 742, 717,
-                958, 609, 842, 451, 688, 753, 854, 685, 93, 857, 440, 380, 126, 721, 328, 753, 470,
-                743, 527};
-
-        for (int i = 0; i < numbers.length; i++) {
-            int el = numbers[i];
-
-            if (el == 237) {
-                break;
-            }
-
-            if (el % 2 == 0) {
-                System.out.println(el);
-            }
-
-        }
-    }
-}
